27d1d180f6fa756303c51287d2f733f8b49ba23a
TheRealVira <therealvira@gmail.com>
2018-10-01 13:37:28 +0200

    started on first example project.

---
+++ XML_homework/GoodBadGuys/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace GoodBadGuys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FormatXml(@"GoodGuyBadGuy.xml"));
+            Console.ReadKey();
+        }
+
+        private static string FormatXml(string xmlFile) => XDocument.Parse(xmlFile).ToString();
+    }
+}
